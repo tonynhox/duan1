e5fa3c7ba323c53f1f0476615a22af970a808772
tonynhox <anhhuytran1470@gmail.com>
2022-11-13 09:55:55 +0700

    serviceAPI

---
+++ duan1/app/src/main/java/com/example/duan1/ServiceAPI.java
@@ -0,0 +1,5 @@
+package com.example.duan1;
+
+public interface ServiceAPI {
+
+}
---
+++ duan1/app/src/main/java/com/example/duan1/models/SanPham.java
@@ -0,0 +1,13 @@
+package com.example.duan1.models;
+
+public class SanPham {
+
+    public int maSp;
+    public String tenSp;
+    public int giaSp;
+    public int maThuongHieu;
+    public String motaSp;
+    public int soLuongSp;
+    public String hinhAnhLon;
+    public String hinhAnhNho;
+}
