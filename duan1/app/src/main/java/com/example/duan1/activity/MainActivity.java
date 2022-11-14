c4780387cfa9e4d912ba29fcd218541e1ca1eee3
NHAT-NNGUYEN <117584551+NHAT-NNGUYEN@users.noreply.github.com>
2022-11-14 07:33:22 +0700

    giao diện

--- duan1/app/src/main/java/com/example/duan1/activity/MainActivity.java
+++ duan1/app/src/main/java/com/example/duan1/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.duan1.activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.duan1.R;
 
@@ -12,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
