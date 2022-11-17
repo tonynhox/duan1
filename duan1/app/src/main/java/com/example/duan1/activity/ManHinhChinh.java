4d643c0871e0cc4f31f54a38f8f61fba74714cb9
tonynhox <anhhuytran1470@gmail.com>
2022-11-17 09:52:21 +0700

    sua giao dien

--- duan1/app/src/main/java/com/example/duan1/activity/ManHinhChinh.java
+++ duan1/app/src/main/java/com/example/duan1/activity/ManHinhChinh.java
@@ -6,11 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.duan1.R;
+import com.example.duan1.fragments.HomeFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class  ManHinhChinh extends AppCompatActivity {
@@ -29,13 +34,42 @@ public class  ManHinhChinh extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_foreground);
         actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_foreground);
+
+        FragmentManager fragmentManager1 = getSupportFragmentManager();
+        Fragment fragment1 = new HomeFragment();
+        fragmentManager1.beginTransaction().replace(R.id.linearLayout, fragment1).commit();
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.dtSS:
+                        Toast.makeText(ManHinhChinh.this, "Sam sung ne", Toast.LENGTH_SHORT).show();
+                        Log.d("hien thi","co du lieu");
+                        break;
+                    case R.id.dtVV:
+                        Toast.makeText(ManHinhChinh.this, "vivo ne", Toast.LENGTH_SHORT).show();
+                        Log.d("hien thi","co du lieu");
+
+                        break;
+                    case R.id.dtIP:
+                        Toast.makeText(ManHinhChinh.this, "iphone ne", Toast.LENGTH_SHORT).show();
+                        Log.d("hien thi","co du lieu");
+
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
+        if (item.getItemId() == android.R.id.home){
             drawerLayout.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
