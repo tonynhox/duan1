36336ff84a86ecf2a48b378d1184c6a373c68673
nga2109 <117584493+nga2109@users.noreply.github.com>
2022-11-19 22:58:10 +0700

    Update TrangThaiDonHangAdapter.java

--- duan1/app/src/main/java/com/example/duan1/adapter/TrangThaiDonHangAdapter.java
+++ duan1/app/src/main/java/com/example/duan1/adapter/TrangThaiDonHangAdapter.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -56,6 +57,7 @@ public class TrangThaiDonHangAdapter extends RecyclerView.Adapter<TrangThaiDonHa
     public class MyViewHolder extends  RecyclerView.ViewHolder{
 
         TextView txtten,txtgia,txtngay,txtsoluong;
+        
         ImageView ivLon;
 
         public MyViewHolder(@NonNull View itemView) {
