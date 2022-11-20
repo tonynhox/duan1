4e9f2716a986a9b98fded2e7042c256c797b3e22
NHAT-NNGUYEN <117584551+NHAT-NNGUYEN@users.noreply.github.com>
2022-11-20 23:19:43 +0700

    chuyển fragment

--- duan1/app/src/main/java/com/example/duan1/activity/ManHinhChinh.java
+++ duan1/app/src/main/java/com/example/duan1/activity/ManHinhChinh.java
@@ -6,31 +6,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.duan1.R;
+import com.example.duan1.fragments.GioHangFragment;
 import com.example.duan1.fragments.HistoryFragment;
+import com.example.duan1.fragments.HoSoCuaToiFragment;
 import com.example.duan1.fragments.HomeFragment;
 import com.example.duan1.fragments.MeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
-public class  ManHinhChinh extends AppCompatActivity {
+public class ManHinhChinh extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    ImageView imageView;
 
-    @Override
+    Button button;
+
+    GioHangFragment gioHangFragment = new GioHangFragment();
+    HoSoCuaToiFragment hoSoCuaToiFragment = new HoSoCuaToiFragment();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manhinhchinh);
+
+        imageView = (ImageView) findViewById(R.id.ivGioHang);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, gioHangFragment).commit();
+            }
+        });
+        setContentView(R.layout.fragment_canhan);
+        button = (Button) findViewById(R.id.btnHoSo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container1,hoSoCuaToiFragment).commit();
+            }
+        });
 
         toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.drawerLayout);
@@ -58,7 +82,6 @@ public class  ManHinhChinh extends AppCompatActivity {
                         Log.d("hien thi", "co du lieu");
                         break;
                 }
-
                 return true;
             }
         });
@@ -82,14 +105,18 @@ public class  ManHinhChinh extends AppCompatActivity {
                     case R.id.me:
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, meFragment).commit();
                         break;
+
                 }
             }
         });
+
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             drawerLayout.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
