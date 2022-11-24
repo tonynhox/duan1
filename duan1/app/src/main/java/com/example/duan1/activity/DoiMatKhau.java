a61a5105c804d532ee93e03f7ce0ec90eb31d307
nga2109 <117584493+nga2109@users.noreply.github.com>
2022-11-24 09:28:20 +0700

    ad

---
+++ duan1/app/src/main/java/com/example/duan1/activity/DoiMatKhau.java
@@ -0,0 +1,16 @@
+package com.example.duan1.activity;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DoiMatKhau extends AppCompatActivity {
+
+
+    EditText txtCurentPassword;
+    EditText txtNewPassword;
+    EditText txtRePassword;
+    Button btnCancel;
+    Button btnAccept;
+}
